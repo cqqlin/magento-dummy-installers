ddf416b59e998a89e27609afaa207195c12f1ace
cqqlin <cqqlin@gmail.com>
2015-08-17 17:24:26 +0930

    #!/bin/bash

--- 1.generate_treefile.sh
+++ 1.generate_treefile.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 if [ ! "$#" -eq 1 ]; then
 	echo "Usage: $0 <path_to_root_directory>"
@@ -6,12 +6,11 @@ if [ ! "$#" -eq 1 ]; then
 fi
 
 ROOT_PATH=$1
-
-
 if [[ ! -d $ROOT_PATH ]]; then
-	echo "$1 is not a directory"
+	echo "$ROOT_PATH is not a directory"
 	exit 0;
 fi
 
+OUTPUT_FILE=treefile-category.txt
 
-java -cp ../tbmangoutils/TBMDataUtils/bin:../tbmangoutils/TBMDataUtils/lib/json-simple-1.1.1.jar com.tbmango.datautil.category.CategoryTreeFileGenerator $ROOT_PATH
+java -cp ../tbmangoutils/TBMDataUtils/bin:../tbmangoutils/TBMDataUtils/lib/json-simple-1.1.1.jar com.tbmango.datautil.category.CategoryTreeFileGenerator $ROOT_PATH > $OUTPUT_FILE
--- 2.start_import.sh
+++ 2.start_import.sh
@@ -1,3 +1,3 @@
-#/bin/bash
+#!/bin/bash
 
 php category_importer.php
